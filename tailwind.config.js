40db1b570c9d2b659c246a4691bf59ef5af38e05
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-01-08 00:31:59 +0700

    add: dependencies material-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import withMT from "@material-tailwind/html/utils/withMT";
 
 /** @type {import('tailwindcss').Config} */
-export default {
+export default withMT({
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -18,4 +19,4 @@ export default {
     },
 
     plugins: [forms],
-};
+});
